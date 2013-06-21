55bcd50ff51606d08c18a04774f4a1c18ae4557c
Matt Kilgore <mattkilgore12@gmail.com>
2013-06-21 01:10:36 -0400

    Adding muttrc

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 h=$HOME
 p=$(pwd)
-declare -a old=('vim' 'vim/vimrc' 'vim/gvimrc' 'X11/Xmodmap' 'Xdefaults'  'xinitrc'  'bash/bashrc' 'bash/inputrc' 'i3'  'i3/i3status.conf'  'xmonad')
+declare -a old=('vim' 'vim/vimrc' 'vim/gvimrc' 'X11/Xmodmap' 'Xdefaults'  'xinitrc'  'bash/bashrc' 'bash/inputrc' 'i3'  'i3/i3status.conf'  'xmonad' 'mutt/muttrc')
 
-declare -a new=('.vim' '.vimrc'   '.gvimrc'    '.Xmodmap'    '.Xdefaults' '.xinitrc' '.bashrc'     '.inputrc'     '.i3' '.i3status.conf'    '.xmonad')
+declare -a new=('.vim' '.vimrc'   '.gvimrc'    '.Xmodmap'    '.Xdefaults' '.xinitrc' '.bashrc'     '.inputrc'     '.i3' '.i3status.conf'    '.xmonad' '.muttrc'   )
 
 # for o, n in old(@), new(@)
 count=${#old[@]}
