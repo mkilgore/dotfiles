e7e83ff57b74f666638af1868a93d7e36533684a
Matt Kilgore <mattkilgore12@gmail.com>
2013-08-14 17:58:48 -0400

    Cleaning things up

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 h=$HOME
 p=$(pwd)
-declare -a old=('vim' 'vim/vimrc' 'vim/gvimrc' 'X11/Xmodmap' 'Xdefaults'  'xinitrc'  'bash/bashrc' 'bash/inputrc' 'i3'  'i3/i3status.conf'  'xmonad' 'mutt/muttrc' 'tmux/tmux.conf')
+declare -a old=('vim' 'vim/vimrc' 'vim/gvimrc' 'X11/Xmodmap' 'Xdefaults'  'xinitrc'  'bash/bashrc' 'bash/inputrc' 'i3'  'i3/i3status.conf'  'xmonad'  'mutt/muttrc' 'tmux/tmux.conf' 'ncmpcpp'  'scripts' 'irssi')
 
-declare -a new=('.vim' '.vimrc'   '.gvimrc'    '.Xmodmap'    '.Xdefaults' '.xinitrc' '.bashrc'     '.inputrc'     '.i3' '.i3status.conf'    '.xmonad' '.muttrc'    '.tmux.conf')
+declare -a new=('.vim' '.vimrc'   '.gvimrc'    '.Xmodmap'    '.Xdefaults' '.xinitrc' '.bashrc'     '.inputrc'     '.i3' '.i3status.conf'    '.xmonad' '.muttrc'     '.tmux.conf'     '.ncmpcpp' 'scripts' '.irssi')
 
 # for o, n in old(@), new(@)
 count=${#old[@]}
